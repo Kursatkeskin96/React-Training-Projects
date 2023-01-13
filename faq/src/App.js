1258d95adf431139ee8261c8a66ca47d524c4750
Kursatkeskin96 <kursatkeskinn@gmail.com>
2023-01-13 12:48:46 +0000

    first commit

--- faq/src/App.js
+++ faq/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [selected, setSelected] = useState(null)
+  const toggle = (i) => {
+    if(selected === i){
+      return setSelected(null)
+    }
+    setSelected(i)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+     <div className="accordion">
+      {data.map((item, i) => (
+      <div className="item">
+        <div className="title" onClick={() => toggle(i)}>
+          <h2>{item.question}</h2>
+          <span>{selected === i ? '-' : '+'}</span>
+        </div>
+        <div className={selected === i ? 'content show' : 'content'}>{item.answer}</div>
+      </div>
+      ))}
+     </div>
     </div>
   );
 }
+
+const data = [
+  {
+    question: "Question 1",
+    answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit eos cupiditate velit. Atque consequatur corrupti adipisci consectetur quia hic optio?",
+
+  },
+  {
+    question: "Question 2",
+    answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit eos cupiditate velit. Atque consequatur corrupti adipisci consectetur quia hic optio?",
+  },
+  {
+    question: "Question 3",
+    answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit eos cupiditate velit. Atque consequatur corrupti adipisci consectetur quia hic optio?",
+  },
+]
 
 export default App;
